3721b32f8a899af1162be92f1d778ac60f664448
LIU Zaoqu <68080738+Zaoqu-Liu@users.noreply.github.com>
2025-10-28 13:56:31 +0800

    Add files via upload

---
+++ fastCNV/R/CNVanalysis.R
@@ -0,0 +1,190 @@
+#' CNVAnalysis
+#' Runs Copy Number Variation (CNV) analysis on a Seurat object or a list of Seurat objects.
+#'
+#' This function performs CNV analysis by calculating genomic scores, applying optional denoising, and optionally
+#' scaling the results based on a reference population. It processes single-cell or spatial transcriptomics data,
+#' generating an additional assay with genomic scores and adding a new metadata column for CNV fractions.
+#'
+#' @param object A Seurat object or a list of Seurat objects containing the data for CNV analysis. Each object
+#' can be either **single-cell** or **spatial transcriptomics** data.
+#' @param referenceVar The name of the metadata column in the Seurat object that contains reference annotations.
+#' @param referenceLabel The label within `referenceVar` that specifies the reference population (can be any type of annotation).
+#' @param pooledReference Logical. If `TRUE` (default), builds a pooled reference across all samples.
+#' @param scaleOnReferenceLabel Logical. If `TRUE` (default), scales the results based on the reference population.
+#' @param assay Name of the assay to run the CNV analysis on. Defaults to the results of `prepareCountsForCNVAnalysis` if available.
+#' @param thresholdPercentile Numeric. Specifies the quantile range to consider (e.g., `0.01` keeps values between the 1st and 99th percentiles). Higher values filter out more background noise.
+#' @param geneMetadata A dataframe containing gene metadata, typically from Ensembl.
+#' @param windowSize Integer. Defines the size of genomic windows for CNV analysis.
+#' @param windowStep Integer. Specifies the step size between genomic windows.
+#' @param saveGenomicWindows Logical. If `TRUE`, saves genomic window information in the current directory (default = `FALSE`).
+#' @param topNGenes Integer. The number of top-expressed genes to retain in the analysis.
+#' @param chrArmsToForce A chromosome arm (e.g., `"8p"`, `"3q"`) or a list of chromosome arms (e.g., `c("3q", "8p", "17p")`) to force into the analysis.
+#' If specified, all genes within the given chromosome arm(s) will be included.
+#' @param genesToForce A list of genes to force into the analysis (e.g. `c("FOXP3","MUC16","SAMD15")`).
+#' @param regionToForce Chromosome region to force into the analysis (vector containing chr, start, end).
+#'
+#' @return If given a **single** Seurat object, returns the same object with:
+#' - An **additional assay** containing genomic scores per genomic window.
+#' - A new **CNV fraction column** added to the object’s metadata.
+#' If given a **list** of Seurat objects, returns the modified list.
+#'
+#' @export
+
+
+CNVAnalysis <- function(object,
+                        referenceVar = NULL,
+                        referenceLabel = NULL,
+                        pooledReference = TRUE,
+                        scaleOnReferenceLabel = TRUE,
+                        assay = NULL,
+                        thresholdPercentile = 0.01,
+                        geneMetadata = getGenes(),
+                        windowSize = 150,
+                        windowStep = 10,
+                        saveGenomicWindows = FALSE,
+                        topNGenes = 7000,
+                        chrArmsToForce = NULL,
+                        genesToForce = NULL,
+                        regionToForce = NULL) {
+  message(crayon::yellow(paste0("[", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "]", " Running CNV analysis...")))
+  if (!is.list(object)) {
+    object <- CNVCalling(object,
+      assay = assay,
+      referenceVar = referenceVar,
+      referenceLabel = referenceLabel,
+      scaleOnReferenceLabel = scaleOnReferenceLabel,
+      thresholdPercentile = thresholdPercentile,
+      geneMetadata = geneMetadata,
+      windowSize = windowSize,
+      windowStep = windowStep,
+      saveGenomicWindows = saveGenomicWindows,
+      topNGenes = topNGenes
+    )
+    invisible(gc())
+  } else {
+    if (length(object) == 1) {
+      object <- list(CNVCalling(object[1],
+        assay = assay,
+        referenceVar = referenceVar,
+        referenceLabel = referenceLabel,
+        scaleOnReferenceLabel = scaleOnReferenceLabel,
+        thresholdPercentile = thresholdPercentile,
+        geneMetadata = geneMetadata,
+        windowSize = windowSize,
+        windowStep = windowStep,
+        saveGenomicWindows = saveGenomicWindows,
+        topNGenes = topNGenes
+      ))
+      invisible(gc())
+    } else {
+      if (pooledReference == TRUE) {
+        object <- CNVCallingList(object,
+          assay = assay,
+          referenceVar = referenceVar,
+          referenceLabel = referenceLabel,
+          scaleOnReferenceLabel = scaleOnReferenceLabel,
+          thresholdPercentile = thresholdPercentile,
+          geneMetadata = geneMetadata,
+          windowSize = windowSize,
+          windowStep = windowStep,
+          saveGenomicWindows = saveGenomicWindows,
+          topNGenes = topNGenes
+        )
+        invisible(gc())
+      } else {
+        object <- lapply(object, function(x) {
+          CNVCalling(x,
+            assay = assay,
+            referenceVar = referenceVar,
+            referenceLabel = referenceLabel,
+            scaleOnReferenceLabel = scaleOnReferenceLabel,
+            thresholdPercentile = thresholdPercentile,
+            geneMetadata = geneMetadata,
+            windowSize = windowSize,
+            windowStep = windowStep,
+            saveGenomicWindows = saveGenomicWindows,
+            topNGenes = topNGenes
+          )
+        })
+        invisible(gc())
+      }
+
+      # if (doRecapPlot == TRUE) {
+      #   message("Plotting the CNV recap heatmap per category accross samples. This could take some time.")
+      #   if (!is.null(referenceVar)) {
+      #   palette <- as.character(paletteer::paletteer_d("ggsci::default_igv"))
+      #   annot_colors <- setNames(palette[1:length(names(object))], names(object))
+      #   uni <- list()
+      #   for (i in names(object)){
+      #     uni[[i]] <- unique(object[[i]]@meta.data[[referenceVar]])
+      #   }
+      #   cellTypes <- unique(unlist(uni))
+      #   cellTypes <- cellTypes[nzchar(cellTypes)]
+      #
+      #   Lmat <- list()
+      #   for (i in cellTypes){
+      #     Lmat[[i]] <- lapply(object, function(x)
+      #       Seurat::Cells(x)[which(x@meta.data[[referenceVar]] == i)])
+      #     Lmat[[i]] <- Lmat[[i]][which(sapply(Lmat[[i]], length)>=5)]
+      #     for (j in names(Lmat[[i]])) {
+      #       Lmat[[i]][[j]] <- as.matrix(Seurat::GetAssay(object[[j]], assay = "genomicScores")["counts"])[,Lmat[[i]][[j]]]
+      #     }
+      #   }
+      #   Lhm <- list()
+      #   for (x in names(Lmat)) {
+      #     modified <- list()
+      #     sample_info <- data.frame(col_name = character(), sample = character(), stringsAsFactors = FALSE)
+      #     for (sample in names(Lmat[[x]])) {
+      #       matrix <- Lmat[[x]][[sample]]
+      #       col_names <- colnames(matrix)
+      #       new_col_names <- paste0(sample, "_", col_names)
+      #       colnames(matrix) <- new_col_names
+      #       modified[[sample]] <- rowMeans(matrix)
+      #
+      #       sample_info <- rbind(sample_info, data.frame(col_name = new_col_names, sample = sample))
+      #     }
+      #     rownames(sample_info) <- sample_info[,1]
+      #     sample_info[,1] <- NULL
+      #     combined <- t(do.call(cbind, modified))
+      #
+      #     sample_df <- as.data.frame(unique(sample_info))
+      #     rownames(sample_df) <- sample_df[,1]
+      #
+      #     Lhm[[x]] <- ComplexHeatmap::pheatmap(combined,
+      #                                          border=T,
+      #                                          border_color = NA,
+      #                                          use_raster = F,
+      #                                          cluster_cols = F,
+      #                                          show_rownames = F,
+      #                                          show_colnames = F,
+      #                                          clustering_distance_rows = "euclidean",
+      #                                          clustering_method = "ward.D",
+      #                                          column_split = as.numeric(sapply(strsplit(colnames(combined),".",fixed=T),function(z)z[1])),
+      #                                          row_split = sample_df,
+      #                                          annotation_row = sample_df,
+      #                                          annotation_colors = list(sample = annot_colors),
+      #                                          col=circlize::colorRamp2(c(-1, 0, 1), c("darkblue", "white", "darkred")))
+      #
+      #   }
+      #   output_file <- "heatmaps_per_annotation.pdf"
+      #
+      #   pdf(output_file, width = 20, height = 10)
+      #   for (i in seq_along(Lhm)){
+      #     grid::grid.newpage()
+      #     grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow = 2, heights = grid::unit.c(grid::unit(1, "cm"),grid::unit(1, "null")))))
+      #     grid::pushViewport(grid::viewport(layout.pos.row = 1))
+      #     grid::grid.text(paste0("CNV heatmap for annotation : ", names(Lhm)[i]), gp = grid::gpar(fontsize = 20))
+      #     grid::popViewport()
+      #     grid::pushViewport(grid::viewport(layout.pos.row = 2))
+      #     ComplexHeatmap::draw(Lhm[[i]], newpage = FALSE)
+      #     grid::popViewport()
+      #
+      #   }
+      #   dev.off()
+      # }
+      # }
+    }
+  }
+  message(crayon::green(paste0("[", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "]", " Done !")))
+  return(object)
+}
---
+++ fastCNV/README.md
@@ -0,0 +1,127 @@
+
+## Introduction <img src = "https://must-bioinfo.github.io/fastCNV/articles/logo.png" align = "right" width = "200">
+
+`fastCNV` is an R package that helps you detect, plot and analyse the putative Copy Number Variations (CNVs) in single cell (scRNA-seq) data or Spatial Transcriptomics (ST) data, including Visium HD. Built on `SeuratObject`, it is easily integrated into scRNA-seq or ST pipelines. 
+
+*WARNING:* Project is still under construction and function usage may change.
+
+## Installation
+
+To install fastCNV, run in R:
+
+```         
+# Previous Version
+remotes::install_github("must-bioinfo/fastCNV")
+
+# Modified Version by Zaoqu Liu
+remotes::install_github("Zaoqu-Liu/fastCNV")
+```
+
+## Usage
+
+Extensive tutorials to run `fastCNV`on scRNA-seq and Spatial Transcriptomics data are available to get started [here](https://must-bioinfo.github.io/fastCNV/articles/index.html).
+
+
+## NEW : RUN fastCNV ON YOUR VISIUM HD DATA
+
+You can now run `fastCNV` on visium HD data using `fastCNV_10XHD()`
+![](https://must-bioinfo.github.io/fastCNV/articles/fastCNV_HD_files/figure-html/cnvheatmapt_clusters-1.png)
+
+
+<img src="https://must-bioinfo.github.io/fastCNV/articles/fastCNV_HD_files/figure-html/plot_cnv_clusters-1.png" width="45%"> <img src="https://must-bioinfo.github.io/fastCNV/articles/fastCNV_HD_files/figure-html/tree_cnv-1.png" width="53%">
+
+
+## FAQ
+
+### 1. What kind of data does fastCNV work on?  
+Currently, fastCNV can be used on **scRNA-seq**, **Visium**, and **Visium HD** data.  
+
+---
+
+### 2. Does it work on mouse data?  
+Not yet — fastCNV currently only works on **human data**. Support for mouse data is in development.  
+
+---
+
+### 3. How long does it take to run?  
+It depends on the dataset size and computational resources:  
+- ~ **1 minute** for small scRNA-seq datasets (~4,000 cells)  
+- ~ **40 minutes** for a Visium HD slide at 16 µm resolution (~150,000 spots)  
+
+---
+
+### 4. I have multiple samples. What’s the fastest way to run fastCNV?  
+You can process all your samples together by providing `fastCNV()` with a list of samples, e.g.:  
+
+``` 
+fastCNV(seuratObj = c(sample1, sample2, sample3, ...),
+        sampleName = c("sample1", "sample2", "sample3", ...),
+        referenceVar = "Annotations",
+        referenceLabel = c("Healthy1", "Healthy2", "Healthy3"))
+```
+
+---
+
+### 5. I have 4 Visium samples, but only one contains healthy tissue. Can I still run them all?  
+Yes. When you provide a list of samples, fastCNV will **pool a reference across all of them**, so the healthy tissue from one slide will be used as a reference for all 4 samples.  
+
+---
+
+### 6. Can I run fastCNV without annotated samples?  
+Yes, fastCNV can run **without a reference**.  
+However, we **highly recommend** using a healthy reference. If you don’t have one, you can download a healthy sample from a database (same organ, same technology) and use it as reference.  
+
+---
+
+### 7. What computational resources are required for Visium HD data?  
+It depends on the bin size:  
+- **16 µm bin size** → ~64 GB RAM is enough  
+- **8 µm bin size** → up to ~200 GB RAM may be required  
+
+We are working on reducing the resource requirements.  
+
+---
+
+### 8. What does the `prepareCounts()` function do?  
+This function is designed for **Visium samples with low read counts**.  
+It aggregates nearby spots until a threshold is reached (typically 3–4 spots).  
+
+---
+
+### 9. How is the heatmap generated?  
+Heatmaps are produced using **ComplexHeatmap**.  
+- You can customize annotation colors with `referencePalette` or `clusterPalette`.  
+- You can also choose which annotations to display and how to split the data.  
+
+---
+
+### 10. How does fastCNV run on Visium HD data?  
+By default, fastCNV uses the **16 µm bin size**. You can increase the resolution, but note that higher resolution requires more computational resources.  
+Apart from binning and clustering, the pipeline works the same way as for other supported data types.  
+
+---
+
+
+
+## Capabilities
+
+`fastCNV` can plot a heatmap of inferred CNVs: 
+
+![](https://must-bioinfo.github.io/fastCNV/articles/fastCNV_sc_files/figure-html/heatmap_sc.png)
+
+It also calculates a `cnv_fraction`, which can be plotted with `Seurat` standard plotting functions:
+
+![](https://must-bioinfo.github.io/fastCNV/articles/fastCNV_sc_files/figure-html/plot_cnv_umap-1.png)
+
+`cnv_fractions` can also be visualized spatially for Spatial Transcriptomics samples:
+
+![](https://must-bioinfo.github.io/fastCNV/articles/fastCNV_ST_files/figure-html/plot_cnv_fraction-1.png )
+
+And `cnv_fractions` can be used to obtain clonal clusters (`cnv_clusters`):
+
+<img src="https://must-bioinfo.github.io/fastCNV/articles/fastCNV_sc_files/figure-html/plot_cnv_clusters-1.png" alt="Clonal clusters for sc data" width="50%">
+
+It also builds a subclonality tree based on the CNV clusters: 
+
+<img src="https://must-bioinfo.github.io/fastCNV/articles/fastCNV_sc_files/figure-html/tree_cnv-1.png" alt="Subclonality tree" width="500">
+
